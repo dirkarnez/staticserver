c4964ecc879ed24b3fee65ff8cedab14da39650f
dirkarnez <smalldirkalex@gmail.com>
2019-06-10 17:55:05 +0800

    - add mode: fs (File System) and spa (Single-page Application)

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "flag"
-  "fmt"
-  "os"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strings"
 )
 
 var (
 	port string
 	root string
+	mode string
 )
 
 func main() {
-  flag.StringVar(&port, "port", "", "Port, default is 9999")
-  flag.StringVar(&root, "root", "", "Absolute path for root directory")
-  flag.Parse()
+	flag.StringVar(&port, "port", "", "Port, default is 9999")
+	flag.StringVar(&root, "root", "", "Absolute path for root directory")
+	flag.StringVar(&mode, "mode", "", "Mode")
+	flag.Parse()
 
-  if len(root) < 1 {
-    dir, err := os.Getwd()
-    if err != nil {
-      log.Fatal(err)
-    }
-    root = dir
-  }
+	if len(root) < 1 {
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		root = dir
+	}
 
-  if len(port) < 1 {
-    port = "80"
-  }
+	if len(port) < 1 {
+		port = "80"
+	}
 
-  http.Handle("/", http.FileServer(http.Dir(root)))
-  log.Println(fmt.Sprintf("Listening on %s, serving %s", port, root))
-  http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if len(mode) < 1 {
+		mode = "fs"
+	}
+
+	switch mode {
+	case "spa":
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			p := r.URL.Path[1:]
+			if strings.Contains(p, ".") {
+				http.ServeFile(w, r, p)
+			} else {
+				http.ServeFile(w, r, path.Join(root, "index.html"))
+			}
+		})
+	case "fs":
+	default:
+		http.Handle("/", http.FileServer(http.Dir(root)))
+	}
+
+	log.Println(fmt.Sprintf("Listening on %s, serving %s", port, root))
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
