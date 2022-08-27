bdd116218188218127bc688c5a7fa3fa269e6f19
dirkarnez <smalldirkalex@gmail.com>
2022-08-28 02:00:59 +0800

    - init 2.0.0, only spa is working

--- go.mod
+++ go.mod
@@ -1,26 +1,51 @@
-module github.com/dirkarnez/staticserver
+module github.com/dirkarnez/staticserver2
 
-go 1.13
+go 1.17
 
 require (
-	github.com/antchfx/jsonquery v0.0.0-20180821084212-a2896be8c82b
-	github.com/antchfx/xpath v1.0.0
-	github.com/gin-contrib/sse v0.1.0
-	github.com/gin-contrib/static v0.0.0-20191128031702-f81c604d8ac2
-	github.com/gin-gonic/gin v1.7.7
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.1 // indirect
-	github.com/golang/protobuf v1.5.2
-	github.com/json-iterator/go v1.1.12
-	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.14
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
-	github.com/modern-go/reflect2 v1.0.2
-	github.com/ugorji/go v1.2.7
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/CloudyKit/jet/v6 v6.1.0 // indirect
+	github.com/Shopify/goreferrer v0.0.0-20210630161223-536fa16abd6f // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/flosch/pongo2/v4 v4.0.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/iris-contrib/jade v1.1.4 // indirect
+	github.com/iris-contrib/schema v0.0.6 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kataras/blocks v0.0.6 // indirect
+	github.com/kataras/golog v0.1.7 // indirect
+	github.com/kataras/iris/v12 v12.2.0-beta4 // indirect
+	github.com/kataras/pio v0.0.10 // indirect
+	github.com/kataras/sitemap v0.0.5 // indirect
+	github.com/kataras/tunnel v0.0.4 // indirect
+	github.com/klauspost/compress v1.15.8 // indirect
+	github.com/mailgun/raymond/v2 v2.0.46 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/schollz/closestmatch v2.1.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tdewolff/minify/v2 v2.12.0 // indirect
+	github.com/tdewolff/parse/v2 v2.6.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/yosssi/ace v0.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2
-	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/ini.v1 v1.66.6 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,46 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
-	"net/http"
+	"mime"
 	"os"
-	"path"
 	"path/filepath"
 
-	"github.com/gin-gonic/gin"
+	"github.com/kataras/iris/v12"
+	"gopkg.in/yaml.v3"
 )
-
-// type Config struct {
-// 	Path      string   `json:"path"`
-// 	Source    string   `json:"source"`
-// 	Template  string   `json:"template"`
-// 	Arguments []string `json:"arguments"`
-// }
 
 var (
 	port uint64
 	root string
 	mode string
-	// configFilePath string
-	// configMap      map[string]Config
-	router *gin.Engine
 )
-
-func init() {
-	// configMap = make(map[string]Config)
-
-	gin.SetMode(gin.ReleaseMode)
-	router = gin.Default()
-}
 
 func main() {
 	flag.StringVar(&root, "root", "", "Absolute path for root directory")
 	flag.Uint64Var(&port, "port", 80, "Port, default is 80")
 	flag.StringVar(&mode, "mode", "", "Mode: fs, spa, upload. Default fs mode")
-	// flag.StringVar(&configFilePath, "config", "", "Config file path")
 	flag.Parse()
 
 	if len(root) < 1 {
@@ -64,160 +46,65 @@ func main() {
 		}
 	}
 
-	// if len(configFilePath) > 0 {
-	// 	raw, err := ioutil.ReadFile(configFilePath)
-	// 	if err != nil {
-	// 		fmt.Println("Config file not found. No configuration is loaded.")
-	// 	}
+	iris.RegisterOnInterrupt(func() {
+		// TODO
+	})
 
-	// 	var configArr []Config
-	// 	if err := json.Unmarshal(raw, &configArr); err != nil {
-	// 		fmt.Println("Cannot parse config file. No configuration is loaded.")
-	// 	}
-
-	// 	for _, config := range configArr {
-	// 		configPath := config.Path
-	// 		configMap[configPath] = config
-	// 		router.POST(configPath, handlerCreator(configPath))
-	// 	}
-	// }
+	app := iris.New()
+	app.Use(iris.Compression)
+	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
+		ctx.Writef("404 not found here")
+	})
 
 	switch mode {
 	case "spa":
-		router.GET("/*page", func(c *gin.Context) {
-			urlPath := c.Request.URL.Path
-			fullPath := filepath.FromSlash(path.Join(root, urlPath))
-			rel, _ := filepath.Rel(root, fullPath)
-
-			if len(rel) > 0 && rel != "." {
-				fileInfo, err := os.Stat(fullPath)
-				if !os.IsNotExist(err) && fileInfo.Mode().IsRegular() {
-					c.File(fullPath)
-					return
-				}
+		app.Use(func(ctx iris.Context) {
+			mimeOverrides := loadMIMEOverrides()
+			extension := filepath.Ext(ctx.RequestPath(true))
+			mimeForExtension, ok := mimeOverrides[extension]
+			if ok {
+				log.Println(extension, mimeForExtension, "user-defined")
+			} else {
+				mimeForExtension := mime.TypeByExtension(extension)
+				log.Println(extension, mimeForExtension, "iris built-in")
 			}
-			c.File(path.Join(root, "index.html"))
-		})
-	case "upload":
-		const tpl = `
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multiple file upload</title>
-	<link rel="stylesheet" href="https://unpkg.com/purecss">
-</head>
-<body>
-<div class="pure-u-1">
-<form class="pure-form pure-form-aligned" action="/upload" method="post" enctype="multipart/form-data">
-   <fieldset>
-        <div class="pure-control-group">
-            <label for="name">Files</label>
-            <input type="file" name="files" multiple>
-        </div>
-        <div class="pure-control-group">
-            <label for="name">Image</label>
-			<input type="file" name="files" multiple accept="image/*" capture>
-        </div>
-        <div class="pure-control-group">
-            <label for="name">Video</label>
-			<input type="file" name="files" multiple accept="video/*" capture>
-        </div>
-        <div class="pure-control-group">
-            <label for="name">Audio</label>
-            <input type="file" name="files" multiple accept="audio/*" capture>
-        </div>
-        <div class="pure-controls">
-			<input class="pure-button pure-button-primary" type="submit" value="Submit">
-        </div>
-    </fieldset>
-</form>
-</div>
-</body>
-</html>`
-		router.SetHTMLTemplate(template.Must(template.New("index").Parse(tpl)))
 
-		router.GET("/", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "index", gin.H{})
+			ctx.ContentType(mimeForExtension)
+			ctx.Next()
 		})
-
-		router.POST("/upload", func(c *gin.Context) {
-			// Multipart form
-			form, _ := c.MultipartForm()
-			files := form.File["files"]
-
-			for _, file := range files {
-				filename := filepath.Base(file.Filename)
-				log.Println(filename)
-				if err := c.SaveUploadedFile(file, filename); err != nil {
-					c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
-					return
-				}
-			}
-			c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
-		})
-	case "fs":
-		router.StaticFS("/", gin.Dir(root, true))
+		app.HandleDir("/", iris.Dir(root))
 	default:
 		log.Fatalf("%s mode is not supported\n", mode)
 	}
 
 	log.Println(fmt.Sprintf("Listening on %d, serving %s, in %s mode", port, root, mode))
-	err := router.Run(fmt.Sprintf(":%d", port))
+	err := app.Listen( // Start the web server at localhost:8080
+		fmt.Sprintf(":%d", port),
+		// skip err server closed when CTRL/CMD+C pressed:
+		iris.WithoutServerError(iris.ErrServerClosed),
+		// enables faster json serialization and more:
+		iris.WithOptimizations,
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-// func handlerCreator(key string) func(c *gin.Context) {
-// 	return func(c *gin.Context) {
-// 		value, ok := configMap[key]
-// 		if !ok {
-// 			c.AbortWithStatus(404)
-// 		} else {
-// 			source := value.Source
-// 			template := value.Template
-// 			arguments := value.Arguments
-
-// 			doc, err := jsonquery.LoadURL(source)
-// 			if err != nil {
-// 				c.AbortWithStatus(404)
-// 			}
-
-// 			if len(template) > 1 {
-// 				argumentsLen := len(arguments)
-
-// 				if argumentsLen > 0 {
-// 					tmpl, err := textTemplate.New("template").Parse(template)
-// 					if err != nil {
-// 						c.AbortWithStatus(404)
-// 						return
-// 					}
-
-// 					buf := new(bytes.Buffer)
-// 					queries := make([]string, argumentsLen)
-// 					for i, argument := range arguments {
-// 						nodeNameNode := jsonquery.FindOne(doc, argument)
-// 						if nodeNameNode != nil {
-// 							queries[i] = nodeNameNode.InnerText()
-// 						} else {
-// 							queries[i] = ""
-// 						}
-// 					}
-
-// 					err = tmpl.Execute(buf, queries)
-// 					if err != nil {
-// 						c.AbortWithStatus(404)
-// 						return
-// 					}
-
-// 					c.JSON(200, buf.String())
-// 				}
-
-// 			} else {
-// 				c.JSON(200, source)
-// 			}
-// 		}
-// 	}
-// }
+func loadMIMEOverrides() map[string]string {
+	m := make(map[string]string)
+	buf := new(bytes.Buffer)
+	file, err := os.Open(filepath.Join(root, "mime.yaml"))
+	if err != nil {
+		return m
+	}
+	defer file.Close()
+	_, err = buf.ReadFrom(file)
+	if err != nil {
+		return m
+	}
+	err = yaml.Unmarshal(buf.Bytes(), &m)
+	if err != nil {
+		return m
+	}
+	return m
+}
